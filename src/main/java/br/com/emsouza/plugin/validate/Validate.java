08ee49f2a9730f9de6865f8e086058c8090f1389
Eduardo Souza <eduardomatosouza@gmail.com>
2013-01-08 18:09:35 -0200

    Share project.

---
+++ src/main/java/br/com/emsouza/plugin/validate/Validate.java
@@ -0,0 +1,19 @@
+package br.com.emsouza.plugin.validate;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author Eduardo Matos de Souza <br>
+ *         13/09/2012 <br>
+ * @goal validate.pom
+ */
+public class Validate extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// TODO Auto-generated method stub
+
+	}
+}
