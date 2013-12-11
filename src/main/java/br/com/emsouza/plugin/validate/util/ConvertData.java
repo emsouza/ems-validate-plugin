a81ca0fea1d9b97360f4f36b8ef21e0c3e7de6db
Eduardo souza <eduardomatosouza@gmail.com>
2013-12-11 10:59:14 -0200

    Change scope dependency to validation.

--- src/main/java/br/com/emsouza/plugin/validate/util/ConvertData.java
+++ src/main/java/br/com/emsouza/plugin/validate/util/ConvertData.java
@@ -3,6 +3,7 @@ package br.com.emsouza.plugin.validate.util;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.maven.plugin.MojoFailureException;
@@ -21,38 +22,47 @@ import br.com.emsouza.plugin.validate.model.Dependency;
  */
 public class ConvertData {
 
-	private static Element rootElement;
+    private static Element rootElement;
 
-	private static Document d;
+    private static Document d;
 
-	public static List<Dependency> readProjectFile(File file) throws MojoFailureException {
-		try {
-			List<Dependency> list = new ArrayList<Dependency>();
+    public static List<Dependency> readProjectFile(File file) throws MojoFailureException {
+        try {
+            List<Dependency> list = new ArrayList<Dependency>();
 
-			rootElement = getContextElement(file);
-			Namespace ns = rootElement.getNamespace();
-			Element dependencies = rootElement.getChild("dependencies", ns);
+            rootElement = getContextElement(file);
+            Namespace ns = rootElement.getNamespace();
+            Element dependencies = rootElement.getChild("dependencies", ns);
 
-			if (dependencies != null) {
-				List<Element> dps = dependencies.getChildren("dependency", ns);
-				for (Element dependency : dps) {
-					Element groupId = dependency.getChild("groupId", ns);
-					Element artifactId = dependency.getChild("artifactId", ns);
-					Element version = dependency.getChild("version", ns);
-					Element scope = dependency.getChild("scope", ns);
-					list.add(new Dependency(groupId.getText(), artifactId.getText(), version.getText(),
-							((scope != null && !scope.getText().isEmpty()) ? scope.getText() : "compile"), null));
-				}
-			}
-			return list;
-		} catch (Exception e) {
-			throw new MojoFailureException("Erro ao validar projeto", e);
-		}
-	}
+            if (dependencies != null) {
+                List<Element> dps = dependencies.getChildren("dependency", ns);
+                for (Element dependency : dps) {
+                    Element groupId = dependency.getChild("groupId", ns);
+                    Element artifactId = dependency.getChild("artifactId", ns);
+                    Element version = dependency.getChild("version", ns);
+                    Element scope = dependency.getChild("scope", ns);
+                    list.add(new Dependency(groupId.getText(), artifactId.getText(), version.getText(),
+                            ((scope != null && !scope.getText().isEmpty()) ? scope.getText() : "compile"), null));
+                }
+            }
 
-	private static Element getContextElement(File file) throws IOException, JDOMException {
-		SAXBuilder sb = new SAXBuilder();
-		d = sb.build(file);
-		return d.getRootElement();
-	}
+            Iterator<Dependency> it = list.iterator();
+            while (it.hasNext()) {
+                Dependency dep = it.next();
+                if (dep.getScope().equalsIgnoreCase("test")) {
+                    it.remove();
+                }
+            }
+
+            return list;
+        } catch (Exception e) {
+            throw new MojoFailureException("Erro ao validar projeto", e);
+        }
+    }
+
+    private static Element getContextElement(File file) throws IOException, JDOMException {
+        SAXBuilder sb = new SAXBuilder();
+        d = sb.build(file);
+        return d.getRootElement();
+    }
 }
